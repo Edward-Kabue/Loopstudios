b2fa998b9826c00b550646d591c26ca90af84385
Edward Kabue <ekabue0@gmail.com>
2022-01-27 18:21:26 +0300

    Add interactive section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
+import Creations from "./components/Creations.component";
 import Hero from "./components/Hero.component";
+import Leader from "./components/Leader.component";
 import Navbar from "./components/Navbar.component";
 
 function App() {
@@ -7,14 +9,13 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      About Careers Events Products Support The leader in interactive VR Founded
-      in 2011, Loopstudios has been producing world-class virtual reality
-      projects for some of the best companies around the globe. Our
-      award-winning creations have transformed businesses through digital
-      experiences that bind to their brand. Our creations See all Deep earth
-      Night arcade Soccer team VR The grid From up above VR Pocket borealis The
-      curiosity Make it fisheye About Careers Events Products Support © 2021
-      Loopstudios. All rights reserved.
+      <Leader />
+      <Creations />
+      About Careers Events Products Support Our award-winning creations have
+      transformed businesses through digital experiences that bind to their
+      brand. Our creations See all Deep earth Night arcade Soccer team VR The
+      grid From up above VR Pocket borealis The curiosity Make it fisheye About
+      Careers Events Products Support © 2021 Loopstudios. All rights reserved.
     </div>
   );
 }
--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../img/logo.svg";
 import hamburger from "../img/icon-hamburger.svg";
-import "../styles/Nav.css";
+
 export default function Navbar() {
   return (
     <div>
